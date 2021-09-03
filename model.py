e2877ae00618c297bbceba6fc0dcd3e23ab3ea9e
Celezcodes <89918905+Celezcodes@users.noreply.github.com>
2021-09-03 19:21:42 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class WaterQuality(BaseModel):
+    ph: float
+    Hardness: float
+    Solids: float
+    Chloramines: float
+    Sulfate: float
+    Conductivity: float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity: float
